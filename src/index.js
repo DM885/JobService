2acb40430b7d73c4dca284f298939286d8e5ed07
EmilOvcina <44057269+EmilOvcina@users.noreply.github.com>
2021-12-19 20:56:48 +0100

    added helpers.

--- src/index.js
+++ src/index.js
@@ -152,7 +152,7 @@ export async function solverHealth(msg, publish){
 
 if(process.env.RAPID)
 {
-    subscriber(host, [
+    helpers.subscriber(helpers.host, [
         {river: "jobs", event: "add-job", work: addJob}, // Adds a new job
         {river: "jobs", event: "queue-check", work: queueCheck}, // Runs the next job in the queue, if there is any
         {river: "jobs", event: "job-history", work: jobHistory}, // Gets the job history of a user
